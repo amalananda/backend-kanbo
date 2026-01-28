829dd107f0626d83ab550e92eed453608a02c594
amala <amalananda332@gmail.com>
2026-01-28 17:42:54 +0700

    feat: add Task entity to task shares module imports

--- src/task-shares/task-shares.module.ts
+++ src/task-shares/task-shares.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { TaskSharesController } from './task-shares.controller'
 import { TaskSharesService } from './task-shares.service'
 import { TaskShare } from '../entities/task-share.entity'
+import { Task } from 'src/entities'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskShare])],
+  imports: [TypeOrmModule.forFeature([TaskShare, Task])],
   controllers: [TaskSharesController],
   providers: [TaskSharesService],
   exports: [TaskSharesService],
